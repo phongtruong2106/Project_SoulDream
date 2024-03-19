819795032094f80a547047448369768704c36eb9
phong truong hoai <truongphong961@gmail.com>
2024-03-20 01:26:19 +0700

    Update

--- Assets/Script/Enemy/EnemyMovement.cs
+++ Assets/Script/Enemy/EnemyMovement.cs
@@ -5,21 +5,38 @@ using UnityEngine;
 public class EnemyMovement : Enemy
 {
     [SerializeField] protected Transform player;
-    [SerializeField] protected float rotationSpeed;
-    [SerializeField] protected float speed;
+    [SerializeField] protected float detectionRadius;
+    [SerializeField] protected Transform handPosition;
+    [SerializeField] protected bool isMove = true;
+    [SerializeField] protected Animator animator;
 
 
     private void Update() {
         this.CheckPlayerInArea();
+        this.CheckTouchPlayerMove();
     }
 
     protected virtual void MoveTowardsPlayer()
     {
+        if(this.isMove)
+        {
+            Vector3 directionToPlayer = player.position - transform.position;
+            Vector3 targetPosition = player.position - directionToPlayer.normalized * detectionRadius;
+            agent.SetDestination(targetPosition);
+        }
+    }
+    
+    protected virtual void CheckTouchPlayerMove()
+    {
         if(enemyController._enemyCheckTouchPlayer._isTouch)
         {
-            agent.SetDestination(player.position);
+            this.isMove = false;
         }
-        
+        if(!this.isMove)
+        {
+            animator.SetTrigger("isHandUp");
+            player.position = handPosition.position;
+        }
     }
 
     protected virtual void CheckPlayerInArea()
