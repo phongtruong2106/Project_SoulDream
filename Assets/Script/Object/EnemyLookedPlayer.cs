af93e8a2add648469f8a8eb843e48958baaf4fe7
phong truong hoai <truongphong961@gmail.com>
2024-05-17 23:38:12 +0700

    Update

--- Assets/Script/Object/EnemyLookedPlayer.cs
+++ Assets/Script/Object/EnemyLookedPlayer.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class EnemyLookedPlayer : NewMonoBehaviour
 {
-        [SerializeField] protected float viewDistance = 10.0f; 
-        [SerializeField] protected float viewAngle = 45.0f; 
-        [SerializeField] private Transform coneOrigin;
+    [SerializeField] protected float viewDistance = 10.0f; 
+    [SerializeField] protected float viewAngle = 45.0f; 
+    [SerializeField] private Transform coneOrigin;
+    [SerializeField] private LayerMask playerLayerMask;
+    [SerializeField] private string playerTag = "Player"; 
 
-
-
-
-
-
+    protected void Update()
+    {
+        this.CheckForPlayer();
+    }
+    private void CheckForPlayer()
+    {
+        Collider[] playersInRange = Physics.OverlapSphere(coneOrigin.position, viewDistance, playerLayerMask);
+        foreach (var player in playersInRange)
+        {
+            if (player.CompareTag(playerTag))
+            {
+                Vector3 directionToPlayer = (player.transform.position - coneOrigin.position).normalized;
+                float angleToPlayer = Vector3.Angle(coneOrigin.forward, directionToPlayer);
+                
+                if (angleToPlayer < viewAngle / 2)
+                {
+                    if (Physics.Raycast(coneOrigin.position, directionToPlayer, out RaycastHit hit, viewDistance, playerLayerMask))
+                    {
+                        if (hit.collider.CompareTag(playerTag))
+                        {
+                            Debug.Log("Player detected");
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     private void OnDrawGizmos()
     {
